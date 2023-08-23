4d1a0ab69b887021358e43901480c0be930caba3
Gaurav Tak <gaurav.tak@roro.io>
2023-08-24 00:45:37 +0530

    Product View added

--- NFCReadDemo-iOS-SwiftUI/views/HomeUi/HomeView.swift
+++ NFCReadDemo-iOS-SwiftUI/views/HomeUi/HomeView.swift
@@ -11,26 +11,25 @@ struct HomeView: View {
     @StateObject var viewModel = HomeViewModel()
     var body: some View {
         VStack {
-            Group {
-                Image("app_icon_small")
-                Text("NFC Demo").font(.system(size: 20))
-                Text("please tap bottom button for NFC card/tag scanning").padding(.top, 8).font(.system(size: 14))
-                if viewModel.isShowProduct {
-                    
-                } else {
-                    Spacer()
-                    Text("No NFC Card is scanned yet.").padding(.top, 64).font(.system(size: 20)).foregroundColor(Color.gray.opacity(0.8))
-                }
-            }.frame(alignment: .center)
-            NFCNotAvailableView(shouldShowNFCNotAvailableView: $viewModel.shouldShowNFCNotAvailableView)
-                .frame(width: 300, height: 240)
-                .opacity(viewModel.shouldShowNFCNotAvailableView ? 1.0 : 0.0)
-                .cornerRadius(10)
-                .shadow(radius: 10)
-            Spacer()
-            NFCScannerBottomView(homeViewModel: viewModel).frame(maxHeight: .infinity, alignment: .bottom)
-        }
-        .padding()
+            Image("app_icon_small")
+            Text("NFC Demo").font(.system(size: 20))
+            Text("please tap bottom button for NFC card/tag scanning").padding(.top, 8).font(.system(size: 14))
+            if viewModel.isShowProduct {
+                ProductView(imageLoader: ImageLoader(urlString: viewModel.product.thumbnail ?? ""), product: $viewModel.product, nfcCardDataValue: $viewModel.lastScannedNFCDataValue)
+            } else {
+                Spacer()
+                Text("No NFC Card is scanned yet.").padding(.top, 64).font(.system(size: 20)).foregroundColor(Color.gray.opacity(0.8))
+            }
+            if viewModel.shouldShowNFCNotAvailableView {
+                NFCNotAvailableView(shouldShowNFCNotAvailableView: $viewModel.shouldShowNFCNotAvailableView)
+                    .frame(width: 300, height: 240)
+                    .opacity(viewModel.shouldShowNFCNotAvailableView ? 1.0 : 0.0)
+                    .cornerRadius(10)
+                    .shadow(radius: 10)
+                Spacer()
+            }
+            NFCScannerBottomView(homeViewModel: viewModel).frame(maxHeight: .infinity, alignment: .bottom).padding(.bottom, 16)
+        }.padding()
     }
 }
 
---
+++ NFCReadDemo-iOS-SwiftUI/views/HomeUi/ProductView.swift
@@ -0,0 +1,56 @@
+//
+//  ProductView.swift
+//  NFCReadDemo-iOS-SwiftUI
+//
+//  Created by Gaurav Tak on 23/08/23.
+//
+
+import SwiftUI
+
+struct ProductView: View {
+    @ObservedObject var imageLoader: ImageLoader
+    @State var image: UIImage = UIImage()
+    @Binding var product: Product
+    @Binding var nfcCardDataValue: String
+    
+    var body: some View {
+        VStack (alignment: .leading) {
+            Text("Scanned NFC Card Value: \(nfcCardDataValue)").font(.system(size: 16)).padding(.top, 8)
+            Text("Scanned Product Details").font(.system(size: 24))
+            
+            HStack {
+                if product.thumbnail != nil && product.thumbnail?.isEmpty == false {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width:120, height:120)
+                        .onReceive(imageLoader.didChange) { data in
+                            self.image = UIImage(data: data) ?? UIImage()
+                        }
+                }
+                Spacer()
+                VStack (alignment: .leading) {
+                    if let title = product.title {
+                        Text("Title : \(title)").fixedSize(horizontal: false, vertical: true)
+                    }
+                    
+                    if let price = product.price {
+                        Text("Price : \(Int(price))").frame(alignment: .leading)
+                        if let discountPercentage = product.discountPercentage {
+                            Text("Discounted Price : \(Int(price - discountPercentage/100.0 * price))")
+                        }
+                    }
+                    if let rating = product.rating {
+                        Text("Rating : \(String(format: "%.2f", rating)) Out of 5")
+                    }
+                }
+            }
+            if let description = product.description {
+                Text("Description : \(description)").multilineTextAlignment(.leading).fixedSize(horizontal: false, vertical: true)
+            }
+            if let brand = product.brand {
+                Text("Brand : \(brand)")
+            }
+        }.padding(.top, 32)
+    }
+}
